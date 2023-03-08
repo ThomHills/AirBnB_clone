5b6905dac589db1fc28bd83a34cd725c8da2efc1
Thom <thomphiri9@gmail.com>
2023-03-08 12:26:21 -0800

    Project commits

--- generate.sh
+++ generate.sh
@@ -10,7 +10,6 @@ set -x
 # see also ".mailmap" for how email addresses and names are deduplicated
 
 cat > "${ROOTDIR}/AUTHORS" <<- EOF
-
 # This file lists all contributors to the repository.
 
 $(git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf)
